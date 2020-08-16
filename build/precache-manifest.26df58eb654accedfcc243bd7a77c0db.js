9f05c4a0e5df5d8cfc6da42c9d83e862eadf7838
Daisy Crego <daisycrego@gmail.com>
2020-08-16 07:59:12 -0400

    installing nodemon

---
+++ build/precache-manifest.26df58eb654accedfcc243bd7a77c0db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eada466354929afc4ce6fb7333280b61",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b208b958a95fc0559609",
+    "url": "/static/css/main.59e235cb.chunk.css"
+  },
+  {
+    "revision": "299ef9be3fd599dfa1e6",
+    "url": "/static/js/2.a3943f07.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a3943f07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b208b958a95fc0559609",
+    "url": "/static/js/main.f6a42b98.chunk.js"
+  },
+  {
+    "revision": "3e16ecf43df84da1085f",
+    "url": "/static/js/runtime-main.b9ef8454.js"
+  }
+]);
