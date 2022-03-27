c5c0fad40d1109084c3853d929b57c40b992fa16
Daisy Crego <cregod@Daisys-MacBook-Pro.local>
2022-03-27 17:26:21 -0400

    Revert "🛠: install material icons, add navbar"
    This reverts commit b1a913ed1e108d2c65a8055b478210c8c553b962.

--- src/app/components/NavBar.js
+++ src/app/components/NavBar.js
@@ -1,10 +1,6 @@
 import React from 'react';
-import AppBar from '@mui/material/AppBar';
+import { Link } from 'react-router-dom';
 import Button from '@mui/material/Button';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
 
 const NavBar = () => {
     const userName = '🌎';
@@ -17,26 +13,17 @@ const NavBar = () => {
     ];
 
     return (
-        <Box sx={{ flexGrow: 1 }}>
-            <AppBar position='fixed' className='nav'>
-                <Toolbar>
-                    <IconButton
-                        size='large'
-                        edge='start'
-                        color='inherit'
-                        aria-label='menu'
-                        sx={{ mr: 2 }}
-                    >
-                        <MenuIcon />
-                    </IconButton>
-                    {buttons.map((item, i) => (
-                        <Button href={item.url} style={{ color: 'white' }}>
+        <div className='nav'>
+            {buttons.map((item, i) => {
+                return (
+                    <Link to={item.url}>
+                        <Button type='button' key={i}>
                             {item.text}
                         </Button>
-                    ))}
-                </Toolbar>
-            </AppBar>
-        </Box>
+                    </Link>
+                );
+            })}
+        </div>
     );
 };
 
